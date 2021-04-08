6458fa53374ca00f08710acf9e4d2b7bd752c79f
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-04-08 09:36:54 +0900

    Refactor : push to map

--- IF/section4/sum_digit.js
+++ IF/section4/sum_digit.js
@@ -1,36 +1,32 @@
 const solution = (numbers) => {
- 
-    const numObjects = [];
     const maxCandidates = [];
+    let maxSum = 0;
+    let maxNum = 0;
 
-    numbers.forEach((el, idx) => {
+    const numObjects = numbers.map((el) => {
         const str = el + '';
         const arr = str.split('');
-        const sum = arr.reduce((acc, cur) =>{
+        const sum = arr.reduce((acc, cur) => {
             return acc += Number(cur);
         }, 0);
-       numObjects.push(
-           {number: el, sum: sum}
-       );
-        
+        return ({ number: el, sum: sum })
     });
-    let maxSum = 0;
-    numObjects.sort((a,b) => b.sum - a.sum);
-    for(let i = 0; i < numObjects.length; i++) {
-        if(numObjects[i].sum >= maxSum) {
+    
+    numObjects.sort((a, b) => b.sum - a.sum);
+    for (let i = 0; i < numObjects.length; i++) {
+        if (numObjects[i].sum >= maxSum) {
             maxSum = numObjects[i].sum;
             maxCandidates.push(numObjects[i]);
         }
-        else break; 
+        else break;
     }
 
-    let maxNum = 0;
     maxCandidates.forEach((el) => {
-        if(el.number > maxNum) maxNum = el.number;
+        if (el.number > maxNum) maxNum = el.number;
     });
-
+    
     return maxNum;
 }
 
-const numbers= [370, 128, 460, 603, 40, 521, 137, 123];
+const numbers = [370, 128, 460, 603, 40, 521, 137, 123];
 solution(numbers);  // 137
