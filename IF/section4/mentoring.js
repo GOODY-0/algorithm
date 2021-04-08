5f55c183c2cb11c3d7e9885ee80c46a827730755
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-04-09 07:19:03 +0900

    Add : 혼자 풀어본 멘토링문제

--- IF/section4/mentoring.js
+++ IF/section4/mentoring.js
@@ -1,31 +1,22 @@
 const solution = (test) => {
-    const students = [];
-    const m = test.length;
-    const n = test[0].length;
-    const answers = [];
     let answer = 0;
-
-    for(let i = 1; i <= n; i++) {
-        for(let j = 1; j <= n; j++) {
-        
+    
+    for(let i = 1; i <= test.length; i++ ){
+        for(let j = 1; j <= test.length; j++) {
             let cnt = 0;
-            for(let k = 0; k < m; k++) {
-                let pi = 0;
-                let pj = 0;
-                for(let s = 0; s < n; s++) {
-                    if(test[k][s] === i) pi = s;
-                    if(test[k][s] === j) pj = s;
+            for(let k = 0; k < test.length; k++) {
+                let pi = pj = 0;
+                for(let s = 0; s < test[0].length; s++) {
+                    if(test[k][s] === i) pi = i;
+                    if(test[k][s] === j) pj = j;
                 }
                 if(pi < pj) cnt++;
             }
-            if(cnt === m) {
-                answer++;
-                answers.push([i, j]);
-            }
+            if(cnt === test.length) answer++;
         }
     }
-    console.log(answers);
-    return (answer);
+    return answer;
+    
 }
 const tests = [[3,4,1,2], [4,3,2,1], [3,1,4,2]];
 solution(tests); 
