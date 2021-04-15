9cac6e9d4e10ee350318acf0b2015e7d07e46749
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-04-15 09:29:45 +0900

    Upload: 인프런 알고리즘 풀이 업로드 * 슬라이딩 윈도우, 해시맵

--- IF/section5/max_sales.js
+++ IF/section5/max_sales.js
@@ -1,21 +1,18 @@
-const solution =(k, sales) => {
-    let max = 0;
+const solution = (k, sales) => {
+  let max = 0;
 
-    for(let i = 0; i < sales.length; i++) {
-        let temp = sales[i];
-        for(let j = i+1; j <= i+2; j++) {
-            if(!sales[j]) break;
-            temp+=sales[j];
-            if(temp > max) max = temp;
-        }
+  for (let i = 0; i < sales.length; i++) {
+    let temp = sales[i];
+    for (let j = i + 1; j <= i + (k - 1); j++) {
+      if (!sales[j]) break;
+      temp += sales[j];
+      if (temp > max) max = temp;
     }
+  }
 
-    // console.log(max);
-    return max;
-}
+  return max;
+};
 
-solution(3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15]);     // 56
+solution(3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15]); // 56
 
-
-38, 46, 56, 55, 55,
-49, 52, 47, 28, 15
+38, 46, 56, 55, 55, 49, 52, 47, 28, 15;
