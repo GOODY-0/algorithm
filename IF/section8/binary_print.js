3522e6812de64d9441db7467fceb831998076686
junzero741 <junzero741@gmail.com>
2021-05-07 07:57:31 +0900

    [Upload]: inflearn algorithm upload

--- IF/section8/binary_print.js
+++ IF/section8/binary_print.js
@@ -1,3 +1,23 @@
-const solution = (num) => {};
+const solution = (num) => {
+    let answer;
+    answer = DFS(num);
+
+    return answer;
+};
+
+const DFS = (num, string = "") => {
+    if (num === 1) {
+        string = num + string;
+        return string;
+    }
+    const remainder = Math.floor(num % 2);
+    const quotient = Math.floor(num / 2);
+    string = remainder + string;
+    return DFS(quotient, string);
+};
 
 solution(11); // 1011
+// 11%2 1
+// 5%2 1
+// 2%2 0
+// 1%2  1
