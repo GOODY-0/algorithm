cb275c87981ff5a462b1ce5d79a31ee58386d7b7
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-05-14 07:55:29 +0900

    [Upload]: section 8

---
+++ IF/section8/coin_exchange.js
@@ -0,0 +1,17 @@
+const solution = (coninTypes, amount) => {
+	let answer;
+	let sum = 0;
+	const DFS = (depth) => {
+		if (sum === amount) return;
+		else {
+		}
+	};
+	console.log(answer);
+	return answer;
+};
+
+solution([1, 2, 5], 15); // 3
+// 1, 2, 5 로 15를 만들 수 있는 모든 경우의 수 ( 각 숫자는 무한정 재활용 가능) 중에서 가장 수가 적은 것..
+// 1 * 15
+// 2 * 7 + 1 * 1
+//
--- IF/section8/overlap_permutation.js
+++ IF/section8/overlap_permutation.js
@@ -1,22 +1,20 @@
 const solution = (N, count) => {
 	let answer = [];
 	let temp = new Array(count);
-	temp.fill(0);
+	temp.fill(1);
 
 	const DFS = (depth) => {
-		if (depth === N) console.log(answer.length);
+		if (depth === count) answer.push([...temp]);
 		else {
-			const d = temp.slice();
 			for (let i = 1; i <= N; i++) {
-				d[depth] = i;
-				answer.push(d);
+				temp[depth] = i;
+				DFS(depth + 1);
 			}
-			DFS(depth + 1);
 		}
 	};
 
-	DFS(1);
+	DFS(0);
 	console.log(answer);
 	return answer;
 };
-solution(3, 2); // 1부터 3까지 적힌 구슬 중 2개 뽑는 경우의 수 (중복허용)
+solution(3, 3); // 1부터 3까지 적힌 구슬 중 2개 뽑는 경우의 수 (중복허용)
