f67448e2a67d0d1f4fe5baf27c4aef3b030f1af7
junzero741 <junzero741@gmail.com>
2021-06-01 09:36:40 +0900

    [Upload]: island DFS

--- IF/section9/island_DFS.js
+++ IF/section9/island_DFS.js
@@ -1,26 +1,32 @@
-const solution = (N, island) => {
+const solution = (island) => {
 	let count = 0;
+	const N = island.length;
 	const dx = [0, 1, 1, 1, 0, -1, -1, -1];
 	const dy = [1, 1, 0, -1, -1, -1, 0, 1];
+
 	const DFS = (x, y) => {
-		if () {       // 깊이 탐색이 끝나는 조건과 다른 섬으로 점프하는 방법에 대해 고민해보자.
-		} else {
-			for (let i = 0; i < dx.length; i++) {
-				const nx = x + dx[i];
-				const ny = x + dy[i];
-				if (nx >= 0 && nx < N && ny >= 0 && ny < N && island[nx][ny] === 1) {
-					island[nx][ny] = 0;
-					DFS(nx, ny);
-					island[nx][ny] = 1;
-				}
+		island[x][y] = 0;
+		for (let k = 0; k < 8; k++) {
+			const nx = x + dx[k];
+			const ny = y + dy[k];
+			if (nx >= 0 && nx < N && ny >= 0 && ny < N && island[nx][ny] === 1) {
+				DFS(nx, ny);
 			}
 		}
 	};
 
-	DFS(0, 0);
+	for (let i = 0; i < N; i++) {
+		for (let j = 0; j < N; j++) {
+			if (island[i][j] === 1) {
+				count++;
+				DFS(i, j);
+			}
+		}
+	}
+	console.log(count);
 };
 
-solution(7, [
+solution([
 	[1, 1, 0, 0, 0, 1, 0],
 	[0, 1, 1, 0, 1, 1, 0],
 	[0, 1, 0, 0, 0, 0, 0],
