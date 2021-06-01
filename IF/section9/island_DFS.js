138d4670edabe2c171f793f1ce3554208b89657a
junzero741 <junzero741@gmail.com>
2021-06-02 08:13:32 +0900

    [Upload]: 섬나라 아일랜드 BFS

--- IF/section9/island_DFS.js
+++ IF/section9/island_DFS.js
@@ -1,15 +1,15 @@
-const solution = (island) => {
-	let count = 0;
-	const N = island.length;
+const solution = (board) => {
+	let island = 0;
+	const N = board.length;
 	const dx = [0, 1, 1, 1, 0, -1, -1, -1];
 	const dy = [1, 1, 0, -1, -1, -1, 0, 1];
 
 	const DFS = (x, y) => {
-		island[x][y] = 0;
+		board[x][y] = 0;
 		for (let k = 0; k < 8; k++) {
 			const nx = x + dx[k];
 			const ny = y + dy[k];
-			if (nx >= 0 && nx < N && ny >= 0 && ny < N && island[nx][ny] === 1) {
+			if (nx >= 0 && nx < N && ny >= 0 && ny < N && board[nx][ny] === 1) {
 				DFS(nx, ny);
 			}
 		}
@@ -17,13 +17,14 @@ const solution = (island) => {
 
 	for (let i = 0; i < N; i++) {
 		for (let j = 0; j < N; j++) {
-			if (island[i][j] === 1) {
-				count++;
+			if (board[i][j] === 1) {
+				island++;
 				DFS(i, j);
 			}
 		}
 	}
-	console.log(count);
+
+	console.log(island);
 };
 
 solution([
