2c49c252ab74983039670033bbc7f47e71666c43
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-06-09 09:41:11 +0900

    [Upload]: 동전교환 다이나믹 프로그래밍

--- IF/section10/coin_exchange.js
+++ IF/section10/coin_exchange.js
@@ -1,20 +1,25 @@
 const solution = (coinKind, exchange) => {
 	let answer = 0;
+	const dy = Array.from({ length: exchange + 1 }, () => 1000);
 
-	const DFS = (coinCnt, sum) => {
-		if (sum > exchange) return;
-		if (sum === exchange) {
-			answer = coinCnt;
-		} else {
-			for (let i = 0; i < coinKind.length; i++) {
-				DFS(coinCnt + 1, coinKind[i] + sum);
-			}
+	dy[0] = 0;
+	for (let i = 0; i < coinKind.length; i++) {
+		for (let j = coinKind[i]; j <= exchange; j++) {
+			dy[j] = Math.min(dy[j - coinKind[i]] + 1, dy[j]);
 		}
-	};
-
-	DFS(0, 0);
+	}
+	answer = dy[exchange];
 	console.log(answer);
 	return answer;
 };
 
 solution([1, 2, 5], 15); // 3
+
+// dy = [0,0,0];
+// [15, 0, 0]
+// [1, 7, 0]
+// [0, 0, 3]
+// 							dy = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+//   1원으로 채우면    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 개 동전 필요
+//   2원으로 채우면    0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8 개 동전 필요
+//   5원으로 채우면    0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3 개 동전 필요
