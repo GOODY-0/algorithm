2128767ed08b3fd3e82795da296c3ae46a3522a2
junzero741 <junzero741@gmail.com>
2021-06-13 11:39:47 +0900

    fix: 코드 수정

--- PR/lv2/pair.js
+++ PR/lv2/pair.js
@@ -1,17 +1,16 @@
 function solution(s) {
+	const arr = [...s];
 	if (s.length % 2 !== 0) return 0;
-	const arr = Array.from(s);
-	while (arr.length) {
-		let flag = 0;
-		for (let i = arr.length - 1; i >= 0; i--) {
-			if (arr[i] === arr[i - 1]) {
-				arr.splice(i - 1, 2);
-				flag = 1;
-			}
+	const stack = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === stack[stack.length - 1]) {
+			stack.pop();
+			continue;
 		}
-		if (flag === 0) return 0;
+		stack.push(arr[i]);
 	}
 
-	return 1;
+	if (stack.length === 0) return 1;
+	else return 0;
 }
-solution("baabaa");
