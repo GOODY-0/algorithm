e958f6f769749376552c6748876eed67d3f52e93
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-06-17 09:59:53 +0900

    [Practice]: DFS 중복순열

--- IF/section8/overlap_permutation.js
+++ IF/section8/overlap_permutation.js
@@ -1,20 +1,20 @@
-const solution = (N, count) => {
-	let answer = [];
-	let temp = new Array(count);
-	temp.fill(1);
-
-	const DFS = (depth) => {
-		if (depth === count) answer.push([...temp]);
-		else {
+const solution = (N, M) => {
+	const tmp = Array.from({ length: M }, () => 0);
+	// let temp = [];
+	const answer = [];
+	const DFS = L => {
+		if (L === M) {
+			answer.push([...tmp]);
+		} else {
 			for (let i = 1; i <= N; i++) {
-				temp[depth] = i;
-				DFS(depth + 1);
+				tmp[L] = i;
+				DFS(L + 1);
 			}
 		}
 	};
 
 	DFS(0);
 	console.log(answer);
-	return answer;
 };
-solution(3, 2); // 1부터 3까지 적힌 구슬 중 2개 뽑는 경우의 수 (중복허용)
+
+solution(3, 2);
