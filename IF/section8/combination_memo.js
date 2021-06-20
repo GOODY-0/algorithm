b56bf9b886a91b61dae98bc36072b7f7ccaa6f50
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-06-20 11:17:29 +0900

    [Upload]: 조합수와 메모이제이션

--- IF/section8/combination_memo.js
+++ IF/section8/combination_memo.js
@@ -1,10 +1,20 @@
 const solution = (n, r) => {
-	const memo = Array.from(Array(35), () => Array(35).fill(0));
+	let answer = 0;
+	const memo = Array.from({ length: 35 }, () =>
+		Array.from({ length: 35 }, () => 0)
+	);
+
 	const DFS = (n, r) => {
-		if (memo[n][r] > 0) return memo[n][r];
+		if (memo[n][r]) return memo[n][r];
 		if (n === r || r === 0) return 1;
-		else return (memo[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+		else {
+			return (memo[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+		}
 	};
+
 	console.log(DFS(n, r));
+	answer = memo[n][r];
+	console.log(answer);
+	return answer;
 };
 solution(5, 3);
