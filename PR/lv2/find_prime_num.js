15c767d4d88c35e664961ec9a0d57380de862a3f
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-01 08:24:14 +0900

    upload: 소수찾기

---
+++ PR/lv2/find_prime_num.js
@@ -0,0 +1,42 @@
+function solution(numbers) {
+	var answer = 0;
+	const numberSplited = numbers.split("");
+
+	const permutations = new Set();
+	const temp = [];
+	const ch = Array.from({ length: numberSplited.length }, () => 0);
+
+	const DFS = (L, M) => {
+		if (L === M) {
+			permutations.add(Number([...temp].join("")));
+		} else {
+			for (let i = 0; i < numberSplited.length; i++) {
+				if (ch[i] === 0) {
+					ch[i] = 1;
+					temp[L] = Number(numberSplited[i]);
+					DFS(L + 1, M);
+					ch[i] = 0;
+				}
+			}
+		}
+	};
+
+	for (let i = 1; i <= numberSplited.length; i++) DFS(0, i);
+
+	const p = Array.from(permutations);
+	p.forEach(el => {
+		if (isPrime(el)) {
+			answer++;
+		}
+	});
+	return answer;
+}
+
+const isPrime = num => {
+	if (num === 2) return true;
+	if (num === 0 || num === 1) return false;
+	for (let i = 2; i * i <= num; i++) {
+		if (num % i === 0) return false;
+	}
+	return true;
+};
