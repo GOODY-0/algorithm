dd45d73f658391b4e5881195f87782a8e538c854
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-06-30 15:01:01 +0900

    upload: 뒤집힌 소수

--- IF/section4/reverse_prime_num.js
+++ IF/section4/reverse_prime_num.js
@@ -1,25 +1,26 @@
-const solution = (numbers) => {
-    const primes = [];
+const solution = nums => {
+	const answers = [];
 
-    const reverseNums = numbers.map((el) => {
-        el += '';
-        el = el.split('');
-        el = el.reverse();
-        el = el.join("");
-        return (Number(el));
-    });
-   
-    
-    reverseNums.forEach((el) => {
-        for(let i = 2; i < Math.sqrt(el); i++) {
-            if(el % i === 0) return;
-        }
-        if(el !== 1) primes.push(el);
-    });
+	const isPrime = num => {
+		if (num === 1) return false;
+		if (num === 2) return true;
+		for (let i = 2; i < Math.floor(Math.sqrt(num)); i++) {
+			if (num % i === 0) return false;
+		}
+		return true;
+	};
 
-    return primes;
-}
+	nums.forEach(num => {
+		let res = 0;
+		while (num) {
+			let t = num % 10;
+			res = res * 10 + t;
+			num = Math.floor(num / 10);
+		}
+		if (isPrime(res)) answers.push(res);
+	});
 
+	console.log(answers);
+};
 
-const numbers = [32, 55, 62, 20, 250, 370, 200, 30, 100];
-solution(numbers);  // [23, 2, 73, 2, 3];
+solution([32, 55, 62, 20, 250, 370, 200, 30, 100]);
