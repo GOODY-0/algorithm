93310dc89900a11597c54eb68baa5de22ea2f368
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-06-30 15:00:46 +0900

    upload: 멘토링

--- IF/section4/mentoring.js
+++ IF/section4/mentoring.js
@@ -1,22 +1,26 @@
-const solution = (test) => {
-    let answer = 0;
-    
-    for(let i = 1; i <= test.length; i++ ){
-        for(let j = 1; j <= test.length; j++) {
-            let cnt = 0;
-            for(let k = 0; k < test.length; k++) {
-                let pi = pj = 0;
-                for(let s = 0; s < test[0].length; s++) {
-                    if(test[k][s] === i) pi = i;
-                    if(test[k][s] === j) pj = j;
-                }
-                if(pi < pj) cnt++;
-            }
-            if(cnt === test.length) answer++;
-        }
-    }
-    return answer;
-    
-}
-const tests = [[3,4,1,2], [4,3,2,1], [3,1,4,2]];
-solution(tests); 
+const solution = exams => {
+	let answer = 0;
+
+	for (let i = 1; i <= exams[0].length; i++) {
+		for (let j = 1; j <= exams[0].length; j++) {
+			let count = 0;
+			for (let k = 0; k < exams.length; k++) {
+				let pi, pj;
+				for (let s = 0; s < exams[0].length; s++) {
+					if (exams[k][s] === i) pi = s;
+					if (exams[k][s] === j) pj = s;
+				}
+				if (pi < pj) count++;
+			}
+			if (count === exams.length) answer++;
+		}
+	}
+
+	console.log(answer);
+};
+
+solution([
+	[3, 4, 1, 2],
+	[4, 3, 2, 1],
+	[3, 1, 4, 2],
+]);
