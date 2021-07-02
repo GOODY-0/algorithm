ac75cc604d230aec10b48b66f9be14fd40e628fe
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-03 07:54:36 +0900

    upload: K번째수

--- PR/lv1/numberK.js
+++ PR/lv1/numberK.js
@@ -1,37 +1,11 @@
-function solution (array, commands) {
-    let answer = [];
+function solution(array, commands) {
+	let answer = [];
+	commands.forEach(command => {
+		const sortedArr = array
+			.slice(command[0] - 1, command[1])
+			.sort((a, b) => a - b);
+		answer.push(sortedArr[command[2] - 1]);
+	});
 
-
-    for(let i = 0; i < commands.length; i++) {
-        let start = commands[i][0]-1;           //array[i], command[i] 고려해서 리팩토링
-        let end = commands[i][1]; 
-        let pick = commands[i][2]-1;
-    
-        let newArray = array.slice(start, end);
-        newArray.sort(function(a,b) {
-            return a-b;
-        });
-        answer.push(newArray[pick]);
-    }
-
-        console.log(answer);
-    
-    return answer;
+	return answer;
 }
-
-
-const array1 = [1,5,2,6,3,7,4];
-const command1 = [[2,5,3],[4,4,1],[1,7,3]];
-
-solution(array1, command1);
-
-
-// for(let i = 0; i < commands.length; i++) {
-//     let start = commands[i][0]-1;           //array[i], command[i] 고려해서 리팩토링
-//     let end = commands[i][1]; 
-//     let pick = commands[i][2]-1;
-
-//     let newArray = array.slice(start, end);
-//     newArray.sort();
-//     answer.push(newArray[pick]);
-// }
