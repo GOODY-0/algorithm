bafa24a7391d137fd3215114ace364ce2c076dc4
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-05 11:56:38 +0900

    upload: 가장 큰 수

--- PR/lv2/biggest_num.js
+++ PR/lv2/biggest_num.js
@@ -1,20 +1,7 @@
 function solution(numbers) {
-	var answer = "";
-	let ns = numbers.map(number => {
-		return (number + "").split("");
-	});
-
-	ns.sort((a, b) => {
-		for (let i = 0; i < 4; i++) {
-			if (!b[i]) return Number(b[0]) - Number(a[i]);
-			else if (!a[i]) return Number(b[i]) - Number(a[0]);
-			else if (Number(b[i]) !== Number(a[i])) {
-				return Number(b[i]) - Number(a[i]);
-			}
-		}
-	});
-	ns = ns.map(el => el.join(""));
-	answer = ns.join("");
-	if (Number(answer) === 0) return "0";
-	return answer;
+	var answer = numbers
+		.map(n => n + "")
+		.sort((a, b) => b + a - (a + b))
+		.join("");
+	return (answer = answer[0] === "0" ? "0" : answer);
 }
