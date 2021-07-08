7aef15e3a1a1fbb0bd39d842bdd963c77b081152
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-08 09:52:52 +0900

    practice: 회의실 배정

--- IF/section7/conf_room.js
+++ IF/section7/conf_room.js
@@ -1,20 +1,28 @@
-const solution = (conf) => {
-    let answer = 0;
-    conf.sort((a,b) => {
-        if(a[1] === b[1]) return a[0] - b[0]; 
-        else return a[1] - b[1];
-    });
-    let et = 0;
-    for (let x of conf) {
-        if(x[0] >= et) {
-            answer++;
-            et = x[1];
-        }
-    }
+const solution = conf => {
+	let answer = 0;
 
-    console.log(answer);
+	conf.sort((a, b) => {
+		if (a[1] === b[1]) return a[0] - b[0];
+		else return a[1] - b[1];
+	});
+
+	let et = 0;
+	for (let times of conf) {
+		if (times[0] >= et) {
+			et = times[1];
+			answer++;
+		}
+	}
+
+	console.log(answer);
 };
 
+function test(arg) {
+	solution(arg);
+}
 
-solution([[1, 4], [2, 3], [3, 5], [4, 6], [5, 7]]); // 3  [2,3], [3,5], [5, 7] 이 회의실을 이용할 수 있다.
-// solution([3, 3], [1, 3], [2, 3]); // 2
+test([
+	[1, 3],
+	[2, 3],
+	[3, 3],
+]);
