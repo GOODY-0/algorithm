830eb615a785e06ecc70765ccaf95719f38ab5a5
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-08 09:15:31 +0900

    refactor: 체육복

--- PR/lv1/sweatSuit.js
+++ PR/lv1/sweatSuit.js
@@ -1,56 +1,36 @@
 function solution(n, lost, reserve) {
-    let answer = 0;
-    lost = lost.sort((a,b) => a-b);
-    reserve = reserve.sort((a,b) => a-b);
-    
+	let answer = n - lost.length;
 
-    for(let i = 0; i < lost.length; i++) {
-        if(reserve.indexOf(lost[i]) !== -1)  {
-            const lostNum = lost.splice(i,1);
-            const idx = reserve.indexOf(lostNum[0]);
-            if(idx !== -1) {
-                reserve.splice(idx,1);
-                i--;
-            }
-        }
-    }
+	reserve.forEach((student, idx, arr) => {
+		const sNum = lost.indexOf(student);
+		if (sNum !== -1) {
+			lost[sNum] = 0;
+			arr[idx] = 0;
+			answer++;
+		}
+	});
 
-    
-    for(let i = 0; i < lost.length; i++) {
-        for(let j = 0; j < reserve.length; j++) {
-            if(lost[i] === reserve[j] + 1 || lost[i] === reserve[j] - 1) {
-                lost.splice(i,1);
-                reserve.splice(j,1);
-                i--;
-                j--;
-            }
-        }
-    }
+	for (let i = 0; i < reserve.length; i++) {
+		for (let j = 0; j < lost.length; j++) {
+			if (!lost[j] || !reserve[i]) continue;
+			if (lost[j] === reserve[i] + 1 || lost[j] === reserve[i] - 1) {
+				reserve[i] = 0;
+				lost[j] = 0;
+				answer++;
+			}
+		}
+	}
 
-
-    
-    
-    answer = n - lost.length;
-    console.log(answer);
-    return answer;
+	return answer;
 }
 
-
-solution(5, [2, 4], [1, 3, 5]);          // 5
-solution(5, [2, 4], [3]);                // 4
-solution(3, [3], [1]);                   // 2
-solution(2, [2], [3]);                   // 2
+solution(5, [2, 4], [1, 3, 5]); // 5
+solution(5, [2, 4], [3]); // 4
+solution(3, [3], [1]); // 2
+solution(2, [2], [3]); // 2
 solution(7, [3, 7], [1, 3, 5]);
-solution(5, [1,2,3,4,5], [3]);
-solution(5, [1,2,3,4,5], [3,4]);
-solution(5, [2,3,6,9], [3,6,12,9]);
-
-
-
-
-
-
-
-
+solution(5, [1, 2, 3, 4, 5], [3]);
+solution(5, [1, 2, 3, 4, 5], [3, 4]);
+solution(5, [2, 3, 6, 9], [3, 6, 12, 9]);
 
 // console.log(lost, reserve);
