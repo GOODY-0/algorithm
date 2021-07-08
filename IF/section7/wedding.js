ec93de529327d6945bf737c6d65bcae56305caec
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-08 12:12:17 +0900

    refactor: 결혼식

--- IF/section7/wedding.js
+++ IF/section7/wedding.js
@@ -1,34 +1,43 @@
-const solution = (arr) => {
-    const answer = [];
-    const visitRecord = [];
-    let counter = 0;
+const solution = people => {
+	let answer = 0;
+	people.sort((a, b) => a[1] - b[1]);
+	let stack = [];
+	let count = 0;
+	let max = 0;
 
-    arr.sort((a,b) => {
-        if(a[0] === b[0]) return a[1] - b[1];
-        else return a[0] - b[0];
-    });
+	for (let times of people) stack.push(["s", times[0]], ["e", times[1]]);
+	stack.sort((a, b) => a[1] - b[1]);
+	stack.forEach(item => {
+		item[0] === "s" ? count++ : count--;
+		max = count > max ? count : max;
+	});
+	answer = max;
+	return answer;
+};
 
-    const people = arr.flat();
-    for(let i = 0; i < people.length; i++) {
-        if(i % 2 === 0) visitRecord.push([people[i],'s']);
-        else visitRecord.push([people[i], 'e']);
-    }
+const testCases = [
+	[
+		[14, 18],
+		[12, 15],
+		[15, 20],
+		[20, 30],
+		[5, 14],
+	],
+	[
+		[14, 18],
+		[12, 15],
+		[15, 20],
+		[20, 30],
+		[5, 14],
+		[21, 24],
+		[22, 23],
+		[1, 29],
+		[2, 3],
+	],
+];
 
-    visitRecord.sort((a,b) => a[0]-b[0]);
-    visitRecord.forEach((record) => {
-        if(record[1] === 's') {
-            counter++;
-            answer.push(counter);
-        }
-        else {
-            counter--;
-            answer.push(counter);
-        }
-    })
-const max = Math.max(...answer);
-    return max;
-    
+function test(cases) {
+	cases.forEach(arg => console.log(solution(arg)));
 }
 
-solution([[14,18], [12,15], [15, 20], [20, 30], [5, 14]]);
-solution([[14,18], [12,15], [15, 20], [20, 30], [5, 14], [21, 24], [22, 23], [1,29], [2,3]]);
+test(testCases);
