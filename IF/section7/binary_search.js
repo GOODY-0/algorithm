1674ea1c92d0bb3762ce98e0b2423d4a960a5108
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-13 09:08:04 +0900

    practice: 이분탐색 연습

--- IF/section7/binary_search.js
+++ IF/section7/binary_search.js
@@ -1,50 +1,23 @@
 const solution = (arr, num) => {
-  arr.sort((a, b) => a - b);
-  let answer;
+	let answer = 0;
+	arr.sort((a, b) => a - b);
+	let left = 0;
+	let right = arr.length - 1;
+	let mid = Math.floor(arr.length / 2);
 
-  const lp = 0;
-  const rp = arr.length - 1;
+	while (arr[mid] !== num) {
+		if (num > arr[mid]) {
+			left = mid + 1;
+		} else if (arr[mid] > num) {
+			right = mid - 1;
+		}
+		mid = Math.floor((left + right) / 2);
+	}
 
-  answer = recursion(arr, num, lp, rp);
-  if (answer === 'None') return 'None';
-  else return answer;
-};
-
-const recursion = (arr, num, lp, rp) => {
-  const center = Math.floor((lp + rp) / 2);
-  if (arr[center] === num) return center;
-  else if (arr[center] > num) rp = center - 1;
-  else if (arr[center] < num) lp = center + 1;
-  if (lp > rp) return 'None';
-  else return recursion(arr, num, lp, rp);
+	answer = mid + 1;
+	console.log(answer);
+	return answer;
 };
 
 solution([7, 28, 43, 67, 88, 92, 100], 100); // 7
 solution([23, 87, 65, 12, 57, 32, 99, 81], 32); // 3
-
-// const solution = (arr, num) => {
-//   arr.sort((a, b) => a - b);
-//   let found = 0;
-//   let answer = 0;
-//   found = dig(arr, num);
-//   console.log(found + 1);
-// };
-
-// const dig = (arr, num) => {
-//   let found = 0;
-//   if (arr.length === 1 && arr[0] === num) found = 0;
-//   else if (arr.length === 1 && arr[0] !== num) found = 'not found';
-//   else {
-//     const center = Math.floor(arr.length / 2);
-//     if (arr[center] > num) {
-//       const subArr = arr.slice(0, center);
-//       found = dig(subArr, num);
-//     } else if (arr[center] < num) {
-//       const subArr = arr.slice(center, arr.length);
-//       found = dig(subArr, num);
-//     } else {
-//       found = center;
-//     }
-//   }
-//   return found;
-// };
