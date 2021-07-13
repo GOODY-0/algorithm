4c6b0a76164f1a8d9cfd96350305c47f351057cd
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-13 09:09:05 +0900

    practice: 최대점수구하기 푼 문제 목록 조건 추가

--- IF/section10/max_score.js
+++ IF/section10/max_score.js
@@ -1,16 +1,30 @@
 const solution = (maxTime, problems) => {
 	let answer = 0;
 	const dy = Array.from({ length: maxTime + 1 }, () => 0);
-
+	let max = 0;
+	const list = [];
 	dy[0] = 0; // 0분이 주어지면 0문제 풀수있고 0점 획득
 	for (let i = 0; i < problems.length; i++) {
 		for (let j = maxTime; j >= problems[i][1]; j--) {
 			// 이미 푼 문제를 또 풀지 않도록 뒤에서부터 순회
 			dy[j] = Math.max(dy[j], dy[j - problems[i][1]] + problems[i][0]);
+			if (dy[j] > max) {
+				max = dy[j];
+				list.push(problems[i]);
+			}
 		}
 	}
 
-	console.log(dy);
+	const newList = [];
+	let stack = 0;
+	for (let x of list) {
+		if (stack + x[1] <= maxTime) {
+			newList.push(x);
+			stack += x[1];
+		}
+	}
+
+	console.log(newList);
 	answer = dy[maxTime];
 	return answer;
 };
