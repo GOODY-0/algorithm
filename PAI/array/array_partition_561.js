ff7d159c20c4ec0344b8e6eaddd3c590359d5ee5
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-26 11:06:21 +0900

    solve: min 함수 없이 최소값 판별

--- PAI/array/array_partition_561.js
+++ PAI/array/array_partition_561.js
@@ -1,15 +1,9 @@
 var arrayPairSum = function (nums) {
 	nums.sort((a, b) => a - b);
 	let result = 0;
-	let current = [];
-
-	nums.forEach(num => {
-		if (current.length < 2) current.push(num);
-		if (current.length === 2) {
-			result += Math.min(...current);
-			current = [];
-		}
-	});
+	for (let i = 0; i < nums.length; i++) {
+		if (i % 2 === 0) result += nums[i];
+	}
 
 	return result;
 };
