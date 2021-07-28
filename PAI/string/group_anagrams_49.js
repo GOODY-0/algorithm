6bc7dc6bd9f5d16cf95ab0ec04b0f2d25425f5d1
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-28 16:03:11 +0900

    solve: sort와 map으로 풀이

--- PAI/string/group_anagrams_49.js
+++ PAI/string/group_anagrams_49.js
@@ -1,17 +1,11 @@
 var groupAnagrams = function (strs) {
 	const result = [];
-	const anagrams = new Map();
-	strs.forEach((str, i) => {
-		const arr = str.split("");
-		arr.sort();
-		const sortedWord = arr.join("");
-		anagrams.get(sortedWord)
-			? anagrams.set(sortedWord, [...anagrams.get(sortedWord), i])
-			: anagrams.set(sortedWord, [i]);
+	const dict = new Map();
+	strs.forEach(str => {
+		// 문자열 원소들로 이뤄진 배열은 그냥 sort() 만 해도 정렬된다.
+		const sortedStr = str.split("").sort().join("");
+		if (!dict.get(sortedStr)) dict.set(sortedStr, [str]);
+		else dict.set(sortedStr, [...dict.get(sortedStr), str]);
 	});
-
-	for (let value of anagrams.values()) {
-		result.push(value.map((el) => strs[el]));
-	}
-	return result;
+	return Array.from(dict.values());
 };
