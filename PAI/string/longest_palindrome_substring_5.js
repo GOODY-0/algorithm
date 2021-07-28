76da1f869a72abe2020798d190e6f4803ba305fa
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-28 17:45:40 +0900

    unsolve: slice와 투포인터

--- PAI/string/longest_palindrome_substring_5.js
+++ PAI/string/longest_palindrome_substring_5.js
@@ -1,22 +1,23 @@
 var longestPalindrome = function (s) {
-	// 문자열 전체가 팰린드롬인 경우 예외처리
-	if (s.split("").reverse().join("") === s) return s;
-	// 문자열 글자수가 1,2 개일 때 예외처리
-	if (s.length <= 2) return s[0];
+	if (s === s.split("").reverse().join("")) return s;
+	if (s.length < 3) return s[0];
+	let max = "";
 
-	// 포인터 양쪽으로 확장하며 팰린드롬 검사
-	function expand(left, right) {
+	const expand = (left, right) => {
 		while (left >= 0 && right < s.length && s[left] === s[right]) {
 			left--;
 			right++;
 		}
+
 		return s.slice(left + 1, right);
+	};
+
+	for (let i = 0; i < s.length; i++) {
+		const odd = expand(i, i + 1);
+		const even = expand(i, i + 2);
+		max = max.length > odd.length ? max : odd;
+		max = max.length > even.length ? max : even;
 	}
 
-	let max = "";
-	for (let i = 0; i < s.length; i++) {
-		max = expand(i, i + 1).length > max.length ? expand(i, i + 1) : max;
-		max = expand(i, i + 2).length > max.length ? expand(i, i + 2) : max;
-	}
 	return max;
 };
