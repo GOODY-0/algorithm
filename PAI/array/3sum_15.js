01f08c0602832c65b543f15fce3f56a6271c4b41
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-30 17:01:05 +0900

    solve: 정렬과 투포인터

--- PAI/array/3sum_15.js
+++ PAI/array/3sum_15.js
@@ -1,27 +1,27 @@
 var threeSum = function (nums) {
-	const answer = [];
-	nums.sort((a, b) => a - b);
-	for (let i = 0; i < nums.length - 2; i++) {
-		if (nums[i] === nums[i - 1]) continue;
+  const map = new Map();
+  nums.sort((a, b) => a - b); // -4 -1 -1 0 1 2
+  const result = [];
+  let fix;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === fix) continue;
+    fix = nums[i];
+    let left = i + 1;
+    let right = nums.length - 1;
 
-		let left = i + 1;
-		let right = nums.length - 1;
-		while (left < right) {
-			const sum = nums[i] + nums[left] + nums[right];
-			if (sum < 0) {
-				left++;
-			} else if (sum > 0) {
-				right--;
-			} else {
-				answer.push([nums[i], nums[left], nums[right]]);
-				while (left < right && nums[left] === nums[left + 1]) left++;
-				while (left < right && nums[right] === nums[right - 1]) right--;
-				// 바로 위에서 중복체크 했으므로 left 와 right 둘 다 이동해도 괜찮음
-				left++;
-				right--;
-			}
-		}
-	}
+    while (left < right) {
+      if (fix + nums[left] + nums[right] < 0) left++;
+      else if (fix + nums[left] + nums[right] > 0) right--;
+      else {
+        if (map.get(fix) !== nums[left]) {
+          result.push([fix, nums[left], nums[right]]);
+          map.set(fix, nums[left]);
+        }
+        left++;
+        right--;
+      }
+    }
+  }
 
-	return answer;
+  return result;
 };
