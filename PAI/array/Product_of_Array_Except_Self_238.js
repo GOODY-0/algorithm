6ca6a7ecac996d7aa6c746df62a618f82e8d9744
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-30 14:31:32 +0900

    solve: 좌우 원소 곱끼리 곱하기

--- PAI/array/Product_of_Array_Except_Self_238.js
+++ PAI/array/Product_of_Array_Except_Self_238.js
@@ -1,19 +1,17 @@
 var productExceptSelf = function (nums) {
-	const result = [];
+  const result = [];
 
-	// nums[i] 의 왼쪽 원소들끼리 곱
-	let p = 1;
-	for (let i = 0; i < nums.length; i++) {
-		result.push(p);
-		p = p * nums[i]; // p 는 지나온 원소들끼리의 곱으로 업데이트
-	}
+  let d = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result.push(d);
+    d *= nums[i];
+  }
 
-	// nums[i] 의 오른쪽 원소들끼리 곱
-	p = 1;
-	for (let i = nums.length - 1; i >= 0; i--) {
-		result[i] = result[i] * p;
-		p = p * nums[i];
-	}
+  d = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] = result[i] * d;
+    d *= nums[i];
+  }
 
-	return result;
+  return result;
 };
