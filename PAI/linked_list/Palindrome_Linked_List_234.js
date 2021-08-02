a9f02ed78f4f2290f623d3c300fa667515ecd808
junzero741 <71166372+junzero741@users.noreply.github.com>
2021-08-02 15:09:01 +0900

    solve: 슬로, 패스트 런너로 링크드 리스트 뒤집어 풀이

--- PAI/linked_list/Palindrome_Linked_List_234.js
+++ PAI/linked_list/Palindrome_Linked_List_234.js
@@ -1,15 +1,39 @@
-var isPalindrome = function (head) {
-  if (!head) return true;
-  let curNode = head;
-  const arr = [];
+var isPalindrome = function(head) {
+    
+    // slow, fast 러너를 이용해 중앙원소를 찾는다.
+    let slow = head;
+    let fast = head;
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    
+    // 주어진 링크드 리스트를 뒤집어서 반환하는 함수
+    const reverse = (curr) => {
+        let prev = null;
+        while(curr) {
+            let temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        
+        return prev;
+    }
+    
+    
+    // head에서 중앙 위치까지 이동하는 firstHalf, 맨 뒤에서 중앙까지 이동하는 secondHalf
+    let firstHalf = head, secondHalf = reverse(slow)
 
-  while (curNode) {
-    arr.push(curNode.val);
-    curNode = curNode.next;
-  }
-
-  while (arr.length > 1) {
-    if (arr.shift() !== arr.pop()) return false;
-  }
-  return true;
+    
+    // 값 비교
+    while(firstHalf && secondHalf) {
+        if(firstHalf.val !== secondHalf.val) return false;
+        firstHalf = firstHalf.next;
+        secondHalf = secondHalf.next;
+    }
+    return true;
+    
+    
 };
