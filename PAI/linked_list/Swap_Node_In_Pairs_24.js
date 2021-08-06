c6b9ee6185bea316fc5ab1d7341a7fae3ace49e8
junzero741 <71166372+junzero741@users.noreply.github.com>
2021-08-06 19:19:17 +0900

    노드 페어 스왑 : 재귀로 풀이 (https://leetcode.com/problems/swap-nodes-in-pairs/)

--- PAI/linked_list/Swap_Node_In_Pairs_24.js
+++ PAI/linked_list/Swap_Node_In_Pairs_24.js
@@ -1,21 +1,9 @@
 var swapPairs = function(head) {
-  var swapPairs = function(head) {
-
-    let prev = new ListNode();
-    let root = prev;
-    prev.next = head;
-    
-    while(head && head.next) {
-        
-        let b = head.next;
-        head.next = b.next;
-        b.next = head;
-        prev.next = b;
-        
-        head = head.next;
-        prev = prev.next.next;
+    if(head && head.next) {
+        let p = head.next;
+        head.next = swapPairs(p.next)
+        p.next = head;
+        return p;
     }
-    
-    return root.next;
-};
+    return head;
 };
