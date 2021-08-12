45a43cd6c77f5244b3082cfd00db8a11b1282a35
junzero741 <71166372+junzero741@users.noreply.github.com>
2021-08-13 01:07:25 +0900

    주식 사고팔기 : 풀이 주석 추가 (https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)

--- PAI/array/Best_Time_To_Buy_and_Sell_Stock_121.js
+++ PAI/array/Best_Time_To_Buy_and_Sell_Stock_121.js
@@ -1,14 +1,19 @@
 var maxProfit = function (prices) {
+  
+  // Input: prices = [7,1,5,3,6,4]
+  // Output: 5
+  
   let min = Number.MAX_SAFE_INTEGER;
   let max = 0;
 
   for (let i = 0; i < prices.length; i++) {
     if (prices[i] < min) {
-      min = prices[i];
-    } else if (prices[i] - min > max) {
-      max = prices[i] - min;
+      min = prices[i];    // 최소값 산정
+    } else if (prices[i] - min > max) { // 현재 값에서 지금까지 나왔던 값중 가장 작은 값(min)을 빼서 최대값과 비교
+      max = prices[i] - min;  // 그 차로 최대값을 업데이트
     }
   }
 
   return max;
 };
+  
