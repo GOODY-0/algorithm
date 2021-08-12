85938e845ed78b4916e7b4b6f5855669aa296521
junzero741 <71166372+junzero741@users.noreply.github.com>
2021-08-13 00:44:35 +0900

    두 수 덧셈 : 주석 해설 추가 (https://leetcode.com/problems/two-sum/)

--- PAI/array/two_sum_1.js
+++ PAI/array/two_sum_1.js
@@ -1,9 +1,12 @@
 var twoSum = function (nums, target) {
+  //Input: nums = [2,7,11,15], target = 9
+  //Output: [0,1]
+  
   const numObj = new Map();
 
   for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-    if (numObj.has(complement)) return [numObj.get(complement), i];
+    const complement = target - nums[i];    // complement가 배열 내에 존재한다는것만 알아내면 됌
+    if (numObj.has(complement)) return [numObj.get(complement), i]; // map 에 complement가 존재하므로 [complement의 인덱스, 현재 값의 인덱스] 를 반환
     numObj.set(nums[i], i);
   }
 };
