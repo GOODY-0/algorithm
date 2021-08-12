a574754ccb57bdff557eecb1b5a98d427b784874
junzero741 <71166372+junzero741@users.noreply.github.com>
2021-08-13 00:22:12 +0900

    배열 파티션 : 풀이 주석 추가(https://leetcode.com/problems/array-partition-i/)

--- PAI/array/Array_Partition_I_561.js
+++ PAI/array/Array_Partition_I_561.js
@@ -1,4 +1,13 @@
 var arrayPairSum = function (nums) {
+  // Input: nums = [1,4,3,2]
+  // Output: 4
+  // 주어진 배열 내 원소를 두 개씩 짝지어 두 수 중 작은 수들만 모아서 다 더했을 때 가장 큰 수를 반환 
+  // min[1,2] + min[3,4] = 4  v
+  // min[1,4] + min[2,3] = 3
+  // min[1,3] + min[2,4] = 3
+  
+  
+  // 정렬한 후 2n 번째 원소들끼리 더하면 된다.
   let max = 0;
   nums.sort((a, b) => a - b);
 
