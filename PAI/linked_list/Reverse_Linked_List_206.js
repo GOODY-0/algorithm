24c76d7124a0be17accaa52254273e7ffe72ea44
junzero741 <71166372+junzero741@users.noreply.github.com>
2021-08-13 22:06:19 +0900

    링크드 리스트 뒤집기 : 풀이 주석 추가 (https://leetcode.com/problems/reverse-linked-list/)

--- PAI/linked_list/Reverse_Linked_List_206.js
+++ PAI/linked_list/Reverse_Linked_List_206.js
@@ -1,9 +1,12 @@
 var reverseList = function(head) {
     
-    if(!head) return null
+    // Input: head = [1,2,3,4,5]
+    // Output: [5,4,3,2,1]
+    
+    if(!head) return null // 빈 리스트면 null 반환 
     let num;
     while(head) {
-        num = new ListNode(head.val, num)
+        num = new ListNode(head.val, num)   // num 에 할당되기전에 우변이 먼저 평가되므로, 우변의 num은 이전 루프에서의 num이다
         head = head.next;
     }
     return num;
