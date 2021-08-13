cfc25ec11cdf00c611805574b44093c1512d39b8
junzero741 <71166372+junzero741@users.noreply.github.com>
2021-08-14 00:39:39 +0900

    노드 페어 스왑 : while 풀이 주석 추가 (https://leetcode.com/problems/swap-nodes-in-pairs/)

--- PAI/linked_list/Swap_Node_In_Pairs_24.js
+++ PAI/linked_list/Swap_Node_In_Pairs_24.js
@@ -1,9 +1,22 @@
 var swapPairs = function(head) {
-    if(head && head.next) {
-        let p = head.next;
-        head.next = swapPairs(p.next)
-        p.next = head;
-        return p;
+    
+    // Input: head = [1,2,3,4]
+    // Output: [2,1,4,3]
+    
+    
+    let prev = new ListNode()
+    let root = prev;
+    prev.next = head;       // 리스트 전체 반환을 위해 루트설정, 앞뒤 스왑을 위해 prev 선언
+    
+    while(head && head.next) {
+        let b = head.next;   // head.next를 변수에 미리 저장
+        head.next = b.next;     // head의 다음을 b의 다음으로 할당해 다다음 노드로 갈 준비
+        b.next = head;          // b의 다음을 head로 할당해 스왑
+        
+        prev.next = b;          // a의 이전 노드가 b를 가리키도록
+        
+        prev = prev.next.next    // 다음 비교를 위해 prev는 두 칸 앞으로 이동
+        head = head.next
     }
-    return head;
+    return root.next;
 };
