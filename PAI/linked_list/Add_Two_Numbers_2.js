9a050b871d42758b18dffa66f0e8e03c56aaaef9
junzero741 <71166372+junzero741@users.noreply.github.com>
2021-08-13 23:08:06 +0900

    두 수 더하기 : 풀이 주석 추가 (https://leetcode.com/problems/add-two-numbers/)

--- PAI/linked_list/Add_Two_Numbers_2.js
+++ PAI/linked_list/Add_Two_Numbers_2.js
@@ -1,7 +1,11 @@
 var addTwoNumbers = function(l1, l2) {
     
+    
+    // Input: l1 = [2,4,3], l2 = [5,6,4]
+    // Output: [7,0,8]
+    
     let head = new ListNode(0);
-    let root = head;
+    let root = head;    // 리스트 전체를 반환해서 보여줘야 하므로 미리 root를 저장해놓는다.
     let carry = 0;
     
     while(l1 || l2 || carry) {
@@ -17,12 +21,13 @@ var addTwoNumbers = function(l1, l2) {
             l2 = l2.next;
         }
         
-        const remainder = (sum+carry) % 10
+        const remainder = (sum+carry) % 10      // 이전 연산에서 가져온 carry 도 합산한다.
         carry = sum + carry >= 10 ? 1 : 0 
         
-        head.next = new ListNode(remainder)
-        head = head.next;
+        head.next = new ListNode(remainder)     
+        head = head.next;                       // 일의자리수로 노드를 만들어 헤드의 다음에 붙인다.
     
     }
     return root.next;
 };
+        
