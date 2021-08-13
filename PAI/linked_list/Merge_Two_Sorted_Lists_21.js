9360d46603c63f961685fb92e3b8deda0dfb0f6b
junzero741 <71166372+junzero741@users.noreply.github.com>
2021-08-13 21:44:00 +0900

    정렬된 두 리스트 병합 : 풀이 주석 추가 (https://leetcode.com/problems/merge-two-sorted-lists/submissions/)

--- PAI/linked_list/Merge_Two_Sorted_Lists_21.js
+++ PAI/linked_list/Merge_Two_Sorted_Lists_21.js
@@ -4,20 +4,24 @@ function ListNode(val, next) {
 }
 
 var mergeTwoLists = function (l1, l2) {
+  
+  // Input: l1 = [1,2,4], l2 = [1,3,4]
+  // Output: [1,1,2,3,4,4]
+  
   let tempNode = new ListNode(0, null);
-  let currentNode = tempNode;
+  let currentNode = tempNode; // 리스트 전체를 반환해야 하므로 tempNode = tempNode.next 로 이동하지 않은, head를 가진 노드가 필요함.
 
   while (l1 && l2) {
-    if (l1.val < l2.val) {
+    if (l1.val < l2.val) {  // 두 노드 중 작은 노드부터 currentNode에 붙음
       currentNode.next = l1;
-      l1 = l1.next;
+      l1 = l1.next;  // currentNode에 노드를 전달해줬으니 다음으로 넘어감
     } else {
       currentNode.next = l2;
       l2 = l2.next;
     }
-    currentNode = currentNode.next;
+    currentNode = currentNode.next; 
   }
 
-  currentNode.next = l1 || l2;
+  currentNode.next = l1 || l2;  // 위 반복문을 마치면 l1과 l2 둘 중 하나는 null이 되고, 나머지 하나는 아닐 것이므로, 아닌 노드를 이어받음
   return tempNode.next;
 };
