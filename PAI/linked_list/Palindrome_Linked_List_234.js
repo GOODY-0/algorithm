dc262c0e8dc8b360303945c04420d05605d75b88
junzero741 <71166372+junzero741@users.noreply.github.com>
2021-08-13 20:32:24 +0900

    좌우대칭 링크드 리스트 : 풀이 주석 추가 (https://leetcode.com/problems/palindrome-linked-list/)

--- PAI/linked_list/Palindrome_Linked_List_234.js
+++ PAI/linked_list/Palindrome_Linked_List_234.js
@@ -1,6 +1,10 @@
 var isPalindrome = function(head) {
     
+    // Input: head = [1,2,2,1]
+    // Output: true
+    
     // slow, fast 러너를 이용해 중앙원소를 찾는다.
+    // fast 러너가 끝에 도달 했을 때 slow 러너는 중간에 도달한다.
     let slow = head;
     let fast = head;
     while(fast && fast.next) {
@@ -13,10 +17,10 @@ var isPalindrome = function(head) {
     const reverse = (curr) => {
         let prev = null;
         while(curr) {
-            let temp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = temp;
+            let temp = curr.next;   // 현재 노드의 다음 노드를 temp 에 저장
+            curr.next = prev;       // 현재 노드의 다음 노드 => 이전 노드
+            prev = curr;            // 현재 노드는 이전 노드에 저장
+            curr = temp;            // 다음 노드로 이동
         }
         
         return prev;
