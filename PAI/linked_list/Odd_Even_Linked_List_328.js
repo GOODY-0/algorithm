2acc069cb44df33fb0e8e17609cffeee38f319b8
junzero741 <71166372+junzero741@users.noreply.github.com>
2021-08-14 02:32:05 +0900

    홀짝 링크드 리스트 : 풀이 주석 추가 (https://leetcode.com/problems/odd-even-linked-list/)

--- PAI/linked_list/Odd_Even_Linked_List_328.js
+++ PAI/linked_list/Odd_Even_Linked_List_328.js
@@ -1,14 +1,21 @@
 var oddEvenList = function(head) {
-    if(!head) return head
+    
+    // Input: head = [1,2,3,4,5]
+    // Output: [1,3,5,2,4]
+    
+    if(!head) return head   // 예외처리
     let odd = head;
     let even = head.next;
-    let even_head = even;
+    let even_head = even;   // 홀짝의 시작노드를 각자 정해주고, 짝수노드의 헤드는 따로 저장해놨다가 홀수노드 끝에 붙인다
     
     while(odd && odd.next) {
+        
+        // even.next만 검사하는 이유는 odd.next는 위 while 조건에서 거르기때문
         if(!even.next) break;
         
-        [odd.next,even.next] = [odd.next.next,even.next.next];
-        [odd,even] = [odd.next,even.next];
+        // odd 와 even 모두 두 칸씩 이동
+        [odd.next, even.next] = [odd.next.next, even.next.next];
+        [odd, even] = [odd.next, even.next];
     }
     odd.next = even_head;
     
