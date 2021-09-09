3dbb8a4664afc7648ba6e5d9cdb2fc2632361347
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-09-09 15:07:07 +0900

    오픈채팅방 : 리팩토링 + 주석 추가

--- PR/lv2/open_chat.js
+++ PR/lv2/open_chat.js
@@ -1,27 +1,25 @@
 function solution(record) {
-  var answer = [];
   const userNameInfo = new Map();
-  const actionMap = {
-    Enter: '님이 들어왔습니다.',
-    Leave: '님이 나갔습니다.',
-  };
+  const actionMap = new Map([
+    ['Enter', '님이 들어왔습니다.'],
+    ['Leave', '님이 나갔습니다.'],
+  ]);
+  const result = [];
 
   // 유저 아이디-이름 매핑
   record.forEach((info) => {
     const arr = parse(info);
-    arr[2] && userNameInfo.set(arr[1], arr[2]);
+    arr[2] && userNameInfo.set(arr[1], arr[2]); //  arr[1] = id  ,  arr[2] = name
   });
 
-  const result = [];
+  // 레코드 순회하며 매핑
   for (let i = 0; i < record.length; i++) {
     const arr = parse(record[i]);
-    const action = actionMap[arr[0]];
+    const action = actionMap.get(arr[0]);
     if (!action) continue;
     const name = userNameInfo.get(arr[1]);
     result.push(`${name}${action}`);
   }
-
-  console.log(result);
 
   return result;
 }
