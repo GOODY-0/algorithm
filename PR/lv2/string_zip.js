e63cf4e982df1b4dc247594c72b879d5559a0084
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-09-09 09:52:27 +0900

    문자열 압축 : 해설보고 풀이(https://haerang94.tistory.com/229)

--- PR/lv2/string_zip.js
+++ PR/lv2/string_zip.js
@@ -1,44 +1,27 @@
 function solution(s) {
-  var answer = 0;
-  let minLength = s.length;
-  let stack = '';
-  const bundleSizes = [];
+  var answer = [s.length];
+  const maxBundleSize = Math.floor(s.length / 2);
 
-  // 몇개 단위로 묶을지 계산해서 bundleSizes 에 push
-  for (let i = 0; i < s.length; i++) {
-    stack += s[i];
-    const back = s.slice(stack.length, stack.length * 2);
-    if (back === stack) bundleSizes.push(stack.length);
+  for (let i = 1; i <= maxBundleSize; i++) {
+    let count = 1;
+    let subStr = '';
+    for (let j = 0; j < s.length; j += i) {
+      const first = s.substring(j, j + i);
+      const second = s.substring(j + i, j + i * 2);
+
+      if (first === second) count++;
+      else {
+        if (count === 1) subStr += first;
+        else {
+          subStr += count + first;
+          count = 1;
+        }
+      }
+    }
+    answer.push(subStr.length);
   }
 
-  // 압축이 하나도 되지 않았을 경우
-  if (bundleSizes.length === 0) return minLength;
-
-  // 압축 사이즈별 문자열 길이 계산
-  bundleSizes.forEach((size) => {
-    let i = 0;
-    let result = '';
-    const bundleResults = [];
-    while (i <= s.length) {
-      const bundle = s.slice(i, i + size);
-      bundleResults.push(bundle);
-      i += size;
-    }
-    let lastBundle;
-    let cnt = 1;
-    bundleResults.forEach((bundle) => {
-      if (bundle === lastBundle) {
-        cnt++;
-      } else if (cnt !== 1) {
-        result += cnt + bundle;
-        cnt = 1;
-      } else {
-        result += bundle;
-      }
-      lastBundle = bundle;
-    });
-    minLength = result.length > minLength ? minLength : result.length;
-  });
-
-  return minLength;
+  return Math.min(...answer);
 }
+
+solution('aababa');
