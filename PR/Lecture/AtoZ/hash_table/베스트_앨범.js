0afc28bc4593005414a2d9a17fffbfccbee75e2d
junzero741 <junzero741@gmail.com>
2022-06-08 09:31:38 +0900

    [SOLVED] : 베스트_앨범 해시테이블로 풀이

--- PR/Lecture/AtoZ/hash_table/베스트_앨범.js
+++ PR/Lecture/AtoZ/hash_table/베스트_앨범.js
@@ -3,15 +3,38 @@
 // 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
 
 function solution(genres, plays) {
-  var answer = [];
-  const totalPlaysPerGenreTable = genres.reduce((acc,cur,idx) => {
-      return {
-        ...acc,
-        [cur] : acc[cur] ? acc[cur] + plays[idx] : plays[idx]}
-  },{})
+   
+  const totalPlaysPerGenreTable = genres.reduce((acc,cur,idx) => (
+      {...acc, [cur] : acc[cur] ? acc[cur] + plays[idx] : plays[idx]}
+  ),{})
   
-  console.log(totalPlaysPerGenreTable);
-  return answer;
+  const sortedSongTable = genres.map((el,idx) => {
+      return [el, plays[idx], idx] 
+  },[]).sort((a,b) => {
+      const sortByGenre = totalPlaysPerGenreTable[b[0]] - totalPlaysPerGenreTable[a[0]]
+      if(sortByGenre === 0) return b[1]-a[1];
+      else return sortByGenre
+  })
+  const genreCountTable = {};
+  
+  const bestAlbum = sortedSongTable.reduce((acc,cur) => {
+      const genre = cur[0]
+      if(!genreCountTable[genre]) {
+          genreCountTable[genre] = 1
+          return [...acc, cur[2]]
+      } else if (genreCountTable[genre] !== 2) {
+          genreCountTable[genre] = genreCountTable[genre] + 1
+          return [...acc, cur[2]]
+      } 
+      else {
+          return [...acc]
+      }
+  },[])
+
+  
+  
+  console.log(bestAlbum);
+  return bestAlbum
 }
 const { msProfiler } = require("../../../../lib/msProfiler");
 
