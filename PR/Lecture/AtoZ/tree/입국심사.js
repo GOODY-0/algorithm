6cafe0b70da7c88c3fd969ed1944224977353729
junzero741 <junzero741@gmail.com>
2022-06-28 10:05:21 +0900

    [SOLVED] : 입국심사 이진탐색 풀이

--- PR/Lecture/AtoZ/tree/입국심사.js
+++ PR/Lecture/AtoZ/tree/입국심사.js
@@ -1,42 +1,25 @@
-function binarySearch (n, arr) {
-  let left = 0;
-  let right = arr.length - 1;
-  
-  while(left <= right) {
-      const mid = Math.floor((left + right) / 2)
-      
-      if(n > arr[mid]) {
-          left = mid + 1;
-      } else {
-          right = mid - 1;
-      }
-      
-      
-      if(n % arr[mid] === 0) return mid;
-  }
-  
-  return -1;
-}   
-
 function solution(n, times) {
-  let time = 0;
-  n -= times.length;
+    // 최소 몇 분에 모든 심사가 끝나는가?
+    // 최소시간과 최대시간을 구해보자.
+    let min = 1;
+    let max = Math.max(...times) * n;
+    
+    // 최소시간과 최대시간 사이 우리가 원하는 값이 있다.
+    // 이진 탐색을 위해 mid 값을 정해보자.
+    while(min <= max) {
+        const mid = Math.floor((min + max) / 2);
+        // mid 분 동안 각 심사관이 처리할 수 있는 입국자의 수를 모두 더해준다.
+        const sum = times.reduce((acc, time) => acc + Math.floor(mid / time), 0)
   
-  
-  // time이 times 배열의 원소중 하나의 배수인가?
-
-  while (n) {
-      time++;
-      if(binarySearch(time, times)) {
-          n--;
-      }
-  }
-  
- 
-  // 가장 빠른 주기를 갖는 카운터에
-  time += times[0]
-  
-  return time;
+        // mid 분 동안 처리할 수 있는 입국자의 수가 실제 입국하려는 사람 수 (n) 보다 크다면, 
+        // 모든 심사를 mid 분 보다는 빨리 끝낼 수 있다는 얘기이므로,
+        // 다음 루프에서 mid 값이 작아지게끔 처리한다.
+        if(sum < n) {
+            min = mid + 1;
+        } else {
+            max = mid - 1;
+        }
+    }
+    
+    return min;
 }
-
-solution(6, [7,10])
